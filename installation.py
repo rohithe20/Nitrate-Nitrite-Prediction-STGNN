4539ec3d3909ade43cd24a8a86c08055fc0cddf8
rohithe20 <109225119+rohithe20@users.noreply.github.com>
2024-04-02 10:54:42 -0400

    Create installation.py

---
+++ installation.py
@@ -0,0 +1,7 @@
+## Installing the torch spatio-temporal library along with requisite binaries 
+
+## tsl installation  
+pip install git+https://github.com/TorchSpatiotemporal/tsl.git 
+
+## torch_scatter and torch_sparse libraries 
+pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.0.0+cpu.html  
